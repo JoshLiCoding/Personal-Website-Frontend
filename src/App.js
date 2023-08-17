5fe3ffd39c753a496aa1572732e8dbbdd0f5c3f5
Josh <joshual2004.work@gmail.com>
2023-08-17 14:56:34 -0400

    changed

--- src/App.js
+++ src/App.js
@@ -14,36 +14,38 @@ import photo from './Assets/photo.jpg';
 
 function App() {
   return (
-    <div className="container mx-10 break-words overflow-auto">
-      <div className='text-5xl mt-10 mb-10'>Joshua Li </div>
-      <img className="float-right w-60 h-60 object-cover rounded-full" src={photo}></img>
-      <div className='text-ml'>
-        <div className='leading-8'>
-          <div>
-            <div className='text-2xl mb-3'>Hey!</div> I am a CS first-year at the University of Waterloo and a recent alumni of Upper Canada College. I am
-            passionate about building impactful technologies through creative problem-solving.
+    <div className="mx-5">
+      <div className='text-5xl my-10'>Joshua Li </div>
+      <div>
+        <div className='text-ml leading-loose'>
+          <div className='text-2xl mb-3'>Hey! 👋</div>
+          <p>
+            You can call me Josh.
             <br></br>
-            Previously, I gained experience with web development in React.js through my internship at Pensieve (Silicon Valley-based startup) as well as through various projects.
-            I have exposure to some competitive programming in Java and artificial intelligence in Python as well.
+            I am a second-year student at the University of Waterloo studying Computer Science and an alumni of Upper Canada College.
+            I am passionate about building impactful technologies, specifically in the areas of AI, distributed computing, and fintech.
             <br></br>
-            In my free time, I love to rock climb and enjoy different genres of music.
+            Currently, I work as a developer @ CGI on cloud and containerization.
+            Previously, I facilitated web development during my internship @ Pensieve (Silicon Valley-based startup).
+            I have programmed competitively and experimented with machine learning as well.
             <br></br>
-            You can find my LinkedIn, GitHub and resume below:
+            In my free time, you can catch me rock climbing, biking around toronto, or reading on philosophy.
+            <br></br>
+            You can find my LinkedIn, GitHub and Resume below:
+          </p>
+        </div>
+        <div className="flex items-baseline mt-4 mb-6">
+          <div className="space-x-6 flex">
+            <a href={'https://www.linkedin.com/in/joshli04/'}><IoLogoLinkedin /></a>
+            <a href={'https://github.com/JoshLiCoding'}><IoLogoGithub /></a>
+            <a href={pdf}><IoMdPerson /></a>
           </div>
         </div>
-
-      </div>
-      <div class="flex items-baseline mt-4 mb-6">
-        <div class="space-x-6 flex">
-          <a href={'https://www.linkedin.com/in/joshli04/'}><IoLogoLinkedin /></a>
-          <a href={'https://github.com/JoshLiCoding'}><IoLogoGithub /></a>
-          <a href={pdf}><IoMdPerson /></a>
-        </div>
       </div>
 
-      <div className='text-4xl mt-10 mb-10 text-center'>Projects</div>
-
-      <div className="grid grid-cols-3 gap-5 w-full">
+      <div className='text-4xl my-10 text-center'>Projects</div>
+      <p className='my-3'>Click on "Website" to visit to the finished product or click anywhere else in the box to visit the GitHub/Devpost page.</p>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 w-full">
         <a href='https://devpost.com/software/polyglot-io' target='_blank'
           className="p-2 border border-solid rounded border-black flex flex-1 flex-col justify-between">
           <div>
@@ -55,10 +57,10 @@ function App() {
                 Polyglot.io
               </div>
               <div className="flex-auto text-sm text-center">
-                NPM-powered video chatting platform for learning new languages
+                Video chat to learn new languages
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
-                React.js, MUI, Socket.IO
+                React.js, MUI, Node.js
               </div>
             </div>
           </div>
@@ -75,10 +77,10 @@ function App() {
                 Company Report
               </div>
               <div className="flex-auto text-sm text-center">
-                Website detailing and graphing stock information
+                Automatically detail and graph stocks
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
-                React.js, Material UI, FinnHub API
+                React.js, MUI, FinnHub API
               </div>
             </div>
           </div>
@@ -102,25 +104,15 @@ function App() {
             </div>
             <div className='mt-3 mx-3'>
               <div className="flex-auto text-2xl font-semibold mb-2 text-center">
-                Notzr
+                NOTZR
               </div>
               <div className="flex-auto text-sm text-center">
-                Free-access notes-sharing platform for Toronto IB students
+                Access free notes from Toronto IB students
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
-                React.js, Chakra UI, Firebase (Realtime Database, Storage)
+                React.js, Chakra UI, Firebase
               </div>
             </div>
-          </div>
-
-          <div className="text-ml text-center mt-2 mb-2">
-            <a target='_blank' href='https://notzr.com/' class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-teal-600 transition duration-300 ease-out border-2 border-teal-500 rounded-full shadow-md group">
-              <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-teal-500 group-hover:translate-x-0 ease">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
-              </span>
-              <span class="absolute flex items-center justify-center w-full h-full text-teal-500 transition-all duration-300 transform group-hover:translate-x-full ease">Website</span>
-              <span class="relative invisible">Website</span>
-            </a>
           </div>
         </a>
 
@@ -135,10 +127,10 @@ function App() {
                 Kanban Board
               </div>
               <div className="flex-auto text-sm text-center">
-                Kanban board with tasks in columns
+                Manage agile tasks with Kanban
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
-                React.js, Material UI, Fetch API
+                React.js, MUI, Fetch API
               </div>
             </div>
           </div>
@@ -155,10 +147,10 @@ function App() {
                 Trie System
               </div>
               <div className="flex-auto text-sm text-center">
-                Web-hosted trie system over CLI that supports client operations
+                Test keyword storage over CLI using a web-hosted trie system
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
-                Node.js, Firebase (Realtime Database)
+                Node.js, Firebase
               </div>
             </div>
           </div>
@@ -175,7 +167,7 @@ function App() {
                 Amnesty UCC
               </div>
               <div className="flex-auto text-sm text-center">
-                Website with activities at UCC's Amnesty International Club (articles, publications, events)
+                View Amnesty club's articles, publications, events
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
                 HTML, Materialize CSS, JS
@@ -184,7 +176,7 @@ function App() {
           </div>
 
           <div className="text-ml text-center mt-2 mb-2">
-            <a target='_blank' href='https://amnestyucc.com/' class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-teal-600 transition duration-300 ease-out border-2 border-teal-500 rounded-full shadow-md group">
+            <a target='_blank' href='https://joshlicoding.github.io/AmnestyUCC/' class="relative inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-teal-600 transition duration-300 ease-out border-2 border-teal-500 rounded-full shadow-md group">
               <span class="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-teal-500 group-hover:translate-x-0 ease">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
               </span>
@@ -205,10 +197,10 @@ function App() {
                 Fenceer
               </div>
               <div className="flex-auto text-sm text-center">
-                Website sharing upcoming Ontario fencing events
+                Upload and access Ontario fencing events
               </div>
               <div className="flex-auto text-xs text-center italic mt-1">
-                HTML, Materialize CSS, JS, Firebase (Realtime Database)
+                HTML, Materialize CSS, JS, Firebase
               </div>
             </div>
           </div>
